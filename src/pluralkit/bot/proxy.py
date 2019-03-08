5a6b6c073b99ae85f92874838815ae50fe67997b
Ske <voltasalt@gmail.com>
2019-03-08 15:42:20 +0100

    Whoops pt. something

--- src/pluralkit/bot/proxy.py
+++ src/pluralkit/bot/proxy.py
@@ -21,6 +21,7 @@ async def get_or_create_webhook_for_channel(conn, bot_user: discord.User, channe
     if webhook_from_db:
         webhook_id, webhook_token = webhook_from_db
 
+        session = channel._state.http._session
         hook = discord.Webhook.partial(webhook_id, webhook_token, adapter=discord.AsyncWebhookAdapter(session))
         return hook
 
