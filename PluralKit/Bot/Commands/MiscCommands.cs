bf8387cf525b0f95c8515adb3de8f36c54c9494e
Ske <voltasalt@gmail.com>
2019-04-29 20:21:16 +0200

    bot: add invite command

---
+++ PluralKit/Bot/Commands/MiscCommands.cs
@@ -0,0 +1,27 @@
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace PluralKit.Bot {
+    public class MiscCommands: ModuleBase<PKCommandContext> {
+        [Command("invite")]
+        [Remarks("invite")]
+        public async Task Invite() {
+            var info = await Context.Client.GetApplicationInfoAsync();
+
+            var permissions = new GuildPermissions(
+                addReactions: true,
+                attachFiles: true,
+                embedLinks: true,
+                manageMessages: true,
+                manageWebhooks: true,
+                readMessageHistory: true,
+                sendMessages: true
+            );
+
+            // TODO: allow customization of invite ID
+            var invite = $"https://discordapp.com/oauth2/authorize?client_id={info.Id}&scope=bot&permissions={permissions.RawValue}";
+            await Context.Channel.SendMessageAsync($"{Emojis.Success} Use this link to add PluralKit to your server:\n<{invite}>");
+        }
+    }
+}
