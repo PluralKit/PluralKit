da8bd20de1f098257627b377e58d4c6748b8aae4
xBelladonna <isabelladonnamoore@outlook.com>
2019-05-06 01:10:15 +0930

    bot: add member count to system card

--- src/pluralkit/bot/embeds.py
+++ src/pluralkit/bot/embeds.py
@@ -109,7 +109,10 @@ async def system_card(conn, client: discord.Client, system: System, is_own_syste
         card.add_field(name="Description",
                        value=truncate_field_body(system.description), inline=False)
 
-    card.add_field(name="Members", value="*See `pk;system {0} list`for the short list, or `pk;system {0} list full` for the detailed list*".format(system.hid) if not is_own_system else "*See `pk;system list` for the short list, or `pk;system list full` for the detailed list*")
+    all_members = await system.get_members(conn)
+    member_count = len(all_members)
+
+    card.add_field(name="Members", value="Count: {}\n*See `pk;system {0} list`for the short list, or `pk;system {0} list full` for the detailed list*".format(member_count, system.hid) if not is_own_system else "Count: {}\n*See `pk;system list` for the short list, or `pk;system list full` for the detailed list*".format(member_count))
     card.set_footer(text="System ID: {}".format(system.hid))
     return card
 
