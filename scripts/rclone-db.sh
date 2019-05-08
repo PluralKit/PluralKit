420e4f80b94e6c3e0043a7107574e056d8cbc88f
baby-princess <46248717+baby-princess@users.noreply.github.com>
2019-05-08 20:34:21 +0000

    Delete rclone-db.sh

--- scripts/rclone-db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Usage: rclone-db.sh <remote>:<path>
-# eg. rclone-db.sh b2:pluralkit
-
-FILENAME=pluralkit-$(date -u +"%Y-%m-%dT%H:%M:%S").sql.gz
-$(dirname $0)/dump-db.sh | gzip | rclone rcat $1/$FILENAME
