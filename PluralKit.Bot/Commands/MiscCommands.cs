823688e63d602260ff640c6f9292ed4406debad3
Ske <voltasalt@gmail.com>
2019-10-27 23:03:20 +0100

    Add pk;flash command

--- PluralKit.Bot/Commands/MiscCommands.cs
+++ PluralKit.Bot/Commands/MiscCommands.cs
@@ -41,6 +41,7 @@ namespace PluralKit.Bot.Commands {
         public Task Thunder(Context ctx) => ctx.Reply("*A giant ball of lightning is conjured and fired directly at your opponent, vanquishing them.*");
         public Task Freeze(Context ctx) => ctx.Reply("*A giant crystal ball of ice is charged and hurled toward your opponent, bursting open and freezing them solid on contact.*");
         public Task Starstorm(Context ctx) => ctx.Reply("*Vibrant colours burst forth from the sky as meteors rain down upon your opponent.*");
+        public Task Flash(Context ctx) => ctx.Reply("*A ball of green light appears above your head and flies towards your enemy, exploding on contact.*");
 
         public async Task Stats(Context ctx)
         {
