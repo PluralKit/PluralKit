335f6203cc4b09452527651d580f1476c0d7d328
Ske <voltasalt@gmail.com>
2020-01-14 12:41:28 +0100

    Restrict pk;random to public members

--- PluralKit.Bot/Commands/MemberCommands.cs
+++ PluralKit.Bot/Commands/MemberCommands.cs
@@ -65,7 +65,7 @@ namespace PluralKit.Bot.Commands
             var randGen = new System.Random(); 
             //Maybe move this somewhere else in the file structure since it doesn't need to get created at every command
 
-            var members = (await _data.GetSystemMembers(ctx.System)).ToList();
+            var members = (await _data.GetSystemMembers(ctx.System)).Where(m => m.MemberPrivacy == PrivacyLevel.Public).ToList();
             if (members == null || !members.Any())
                 throw Errors.NoMembersError;
             var randInt = randGen.Next(members.Count);
