93e53be7db228c94bc7dd1ab75c3d69f98277f35
Ske <voltasalt@gmail.com>
2020-04-09 17:25:01 +0200

    Bump member count limit for the time being

--- PluralKit.Core/Utils/Limits.cs
+++ PluralKit.Core/Utils/Limits.cs
@@ -5,7 +5,7 @@ namespace PluralKit.Core {
 
         public static readonly int MaxSystemNameLength = 100;
         public static readonly int MaxSystemTagLength = MaxProxyNameLength - 1;
-        public static readonly int MaxMemberCount = 1000;
+        public static readonly int MaxMemberCount = 1500;
         public static readonly int MaxMembersWarnThreshold = MaxMemberCount - 50;
         public static readonly int MaxDescriptionLength = 1000;
         public static readonly int MaxMemberNameLength = 100; // Fair bit larger than MaxProxyNameLength for bookkeeping
