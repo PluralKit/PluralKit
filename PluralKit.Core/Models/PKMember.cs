23c595f675e7ae90c070b9301881d0cd95f264ce
Ske <voltasalt@gmail.com>
2020-06-13 13:18:21 +0200

    Extract ProxyTag to separate file

--- PluralKit.Core/Models/PKMember.cs
+++ PluralKit.Core/Models/PKMember.cs
@@ -46,34 +46,4 @@ namespace PluralKit.Core {
             return $"{guildDisplayName ?? DisplayName ?? Name} {systemTag}";
         }
     }
-    
-    public struct ProxyTag
-    {
-        public ProxyTag(string prefix, string suffix)
-        {
-            // Normalize empty strings to null for DB
-            Prefix = prefix?.Length == 0 ? null : prefix;
-            Suffix = suffix?.Length == 0 ? null : suffix;
-        }
-
-        [JsonProperty("prefix")] public string Prefix { get; set; }
-        [JsonProperty("suffix")] public string Suffix { get; set; }
-
-        [JsonIgnore] public string ProxyString => $"{Prefix ?? ""}text{Suffix ?? ""}";
-
-        public bool IsEmpty => Prefix == null && Suffix == null;
-
-        public bool Equals(ProxyTag other) => Prefix == other.Prefix && Suffix == other.Suffix;
-
-        public override bool Equals(object obj) => obj is ProxyTag other && Equals(other);
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                return ((Prefix != null ? Prefix.GetHashCode() : 0) * 397) ^
-                       (Suffix != null ? Suffix.GetHashCode() : 0);
-            }
-        }
-    }
 }
