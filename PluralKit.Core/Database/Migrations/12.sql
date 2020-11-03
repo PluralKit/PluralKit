f738ca166bbee97fc2b639927e7d91638d789f33
spiral <spiral@spiral.sh>
2020-11-03 01:34:26 -0500

    add changelog message

--- PluralKit.Core/Database/Migrations/12.sql
+++ PluralKit.Core/Database/Migrations/12.sql
@@ -1,5 +1,6 @@
 -- SCHEMA VERSION 12: <insert date> --
 -- Add disabling front/latch autoproxy per-member --
+-- Add configurable latch timeout duration -- 
 
 alter table members add column disable_autoproxy bool not null default false;
 alter table systems add column latch_timeout int not null default 6;
