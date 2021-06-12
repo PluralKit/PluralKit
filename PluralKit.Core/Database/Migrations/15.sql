fcb72694100a05a8022d4b4f6c3108f893b46de7
Lucca Kaasgaard Christiansen <1078059@ucn.dk>
2021-06-12 16:56:42 +0200

    merge to address confligt with migration

---
+++ PluralKit.Core/Database/Migrations/15.sql
@@ -0,0 +1,15 @@
+-- SCHEMA VERSION 14: 2020-04-02 --
+-- added reminders table
+
+CREATE TABLE IF NOT EXISTS reminders
+(
+    mid bigint NOT NULL PRIMARY KEY,
+    channel bigint NOT NULL,
+    guild bigint,
+    member integer REFERENCES members (id) ON DELETE CASCADE,
+    system integer REFERENCES systems (id) ON DELETE CASCADE NOT NULL,
+    seen boolean NOT NULL default false,
+    timestamp timestamp NOT NULL default (current_timestamp at time zone 'utc')
+);
+
+update info set schema_version = 15; 
