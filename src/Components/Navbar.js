5ce7a74d28300e09ce82866b62308ffbdf1fa067
Spectralitree <72747870+Spectralitree@users.noreply.github.com>
2021-06-26 23:06:22 +0200

    more comments

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -24,6 +24,7 @@ const Navbar = ({ setIsSubmit, forceUpdate}) => {
                 pk-webs
             </BS.Navbar.Brand>
             <BS.NavDropdown id="menu" className="mr-auto" title="Menu">
+            {/* for some reason just using react router's link elements doesn't work here, maybe look into that */}
             <BS.NavDropdown.Item onClick={() => history.push('/pk-webs/dash')} >Dash</BS.NavDropdown.Item>
             <BS.NavDropdown.Item onClick={() => history.push('/pk-webs/settings')} >Settings</BS.NavDropdown.Item>
             <BS.NavDropdown.Item onClick={() => history.push('/pk-webs/profile')}>Public profile</BS.NavDropdown.Item>
