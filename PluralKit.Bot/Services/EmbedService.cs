329ae5c3785ab52d5cbc8c084032e6472c3aa41f
Starshine System <us@starshines.xyz>
2021-07-19 01:11:53 +0200

    deduplicate code, reformat

--- PluralKit.Bot/Services/EmbedService.cs
+++ PluralKit.Bot/Services/EmbedService.cs
@@ -301,11 +301,10 @@ namespace PluralKit.Bot {
                 if (member != null)
                     // Don't do an extra request if we already have this info from the member lookup
                     userInfo = member.User;
-                else userInfo = await _cache.GetOrFetchUser(_rest, msg.Message.Sender);
-
                 memberInfo = member;
             }
-            else userInfo = await _cache.GetOrFetchUser(_rest, msg.Message.Sender);
+
+            if (userInfo == null) userInfo = await _cache.GetOrFetchUser(_rest, msg.Message.Sender);
 
             // Calculate string displayed under "Sent by"
             string userStr;
@@ -329,7 +328,9 @@ namespace PluralKit.Bot {
             if (roles != null && roles.Count > 0)
             {
                 // TODO: what if role isn't in cache? figure out a fallback
-                var rolesString = string.Join(", ", roles.Select(id => _cache.GetRole(id)).OrderByDescending(role => role.Position).Select(role => role.Name));
+                var rolesString = string.Join(", ", roles.Select(id => _cache.GetRole(id))
+                    .OrderByDescending(role => role.Position)
+                    .Select(role => role.Name));
                 eb.Field(new($"Account roles ({roles.Count})", rolesString.Truncate(1024)));
             }
             
