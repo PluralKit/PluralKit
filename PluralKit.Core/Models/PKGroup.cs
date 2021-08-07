26c18d2f484df796655356b7a8a1f3efd0830b9a
spiral <spiral@spiral.sh>
2021-08-07 16:39:32 -0400

    refactor: move modeltypes to own folder, move IDs to file of respective type

--- PluralKit.Core/Models/PKGroup.cs
+++ PluralKit.Core/Models/PKGroup.cs
@@ -1,8 +1,34 @@
 ﻿using NodaTime;
 
-#nullable enable
+
+
 namespace PluralKit.Core
 {
+    public readonly struct GroupId: INumericId<GroupId, int>
+    {
+        public int Value { get; }
+
+        public GroupId(int value)
+        {
+            Value = value;
+        }
+
+        public bool Equals(GroupId other) => Value == other.Value;
+
+        public override bool Equals(object obj) => obj is GroupId other && Equals(other);
+
+        public override int GetHashCode() => Value;
+
+        public static bool operator ==(GroupId left, GroupId right) => left.Equals(right);
+
+        public static bool operator !=(GroupId left, GroupId right) => !left.Equals(right);
+
+        public int CompareTo(GroupId other) => Value.CompareTo(other.Value);
+        
+        public override string ToString() => $"Group #{Value}";
+    }
+
+#nullable enable
     public class PKGroup
     {
         public GroupId Id { get; private set; }
--- PluralKit.Core/Models/PKMember.cs
+++ PluralKit.Core/Models/PKMember.cs
@@ -5,7 +5,33 @@ using Newtonsoft.Json;
 using NodaTime;
 using NodaTime.Text;
 
+
+
 namespace PluralKit.Core {
+    public readonly struct MemberId: INumericId<MemberId, int>
+    {
+        public int Value { get; }
+
+        public MemberId(int value)
+        {
+            Value = value;
+        }
+
+        public bool Equals(MemberId other) => Value == other.Value;
+
+        public override bool Equals(object obj) => obj is MemberId other && Equals(other);
+
+        public override int GetHashCode() => Value;
+
+        public static bool operator ==(MemberId left, MemberId right) => left.Equals(right);
+
+        public static bool operator !=(MemberId left, MemberId right) => !left.Equals(right);
+
+        public int CompareTo(MemberId other) => Value.CompareTo(other.Value);
+        
+        public override string ToString() => $"Member #{Value}";
+    }
+
     public class PKMember
     {
         // Dapper *can* figure out mapping to getter-only properties, but this doesn't work
