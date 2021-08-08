3fea63302dd21bd55174c0715cf104513c70e7a9
Spectralitree <72747870+Spectralitree@users.noreply.github.com>
2021-08-08 12:16:56 +0200

    remove unused useEffect hook

--- src/Pages/MemberPages.js
+++ src/Pages/MemberPages.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import * as BS from 'react-bootstrap';
 import { useParams } from "react-router-dom";
 import MemberPage from '../Components/Private/MemberPage.js'
@@ -10,11 +10,6 @@ export default function MemberPages(props) {
     const memberpages = props.members.filter((member) => member.id === memberID)
     const memberpage = memberpages.map((member) => <MemberPage key={member.id} member={member} edit={props.edit}/>)
     const noMatch = memberpages.length === 0;
-
-    useEffect (() => { 
-        if (memberpages.length === 0) {
-         }
-    }, [memberpages])
 
     if (noMatch) return (
         <BS.Alert variant="danger">You do not have a member with the ID '{memberID}' in your system. Please check the ID again.</BS.Alert>
--- src/Pages/ProfilePages.js
+++ src/Pages/ProfilePages.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import * as BS from 'react-bootstrap';
 import { useParams } from "react-router-dom";
 import ProfilePage from '../Components/Public/ProfilePage.js'
@@ -7,13 +7,8 @@ export default function MemberPages(props) {
     const { memberID } = useParams();
     
     const memberpages = props.members.filter((member) => member.id === memberID)
-    const memberpage = memberpages.map((member) => <ProfilePage key={member.id} member={member}/>)
+    const memberpage = memberpages.map((member) => <ProfilePage key={member.id} member={member} list={true}/>)
     const noMatch = memberpages.length === 0;
-
-    useEffect (() => { 
-        if (memberpages.length === 0) {
-         }
-    }, [memberpages])
 
     if (noMatch) return (
         <BS.Alert variant="danger">This system does not have a member with the ID '{memberID}', or the member's visibility is set to private.</BS.Alert>
