83212b4bb2087b7d2e9b38658c0c6a6fa2928baa
Ske <voltasalt@gmail.com>
2021-08-24 11:22:41 +0200

    Don't override maximum pool size from connection string

--- PluralKit.Core/Database/Database.cs
+++ PluralKit.Core/Database/Database.cs
@@ -37,7 +37,7 @@ namespace PluralKit.Core
             
             _connectionString = new NpgsqlConnectionStringBuilder(_config.Database)
             {
-                Pooling = true, MaxPoolSize = 500, Enlist = false, NoResetOnClose = true,
+                Pooling = true, Enlist = false, NoResetOnClose = true,
                 
                 // Lower timeout than default (15s -> 2s), should ideally fail-fast instead of hanging
                 Timeout = 2
