4064078309cc61f70c905655c2237d7c4d55618f
spiral <spiral@spiral.sh>
2021-12-01 17:09:32 -0500

    fix: document member/group default privacy correctly

--- PluralKit.Bot/CommandMeta/CommandHelp.cs
+++ PluralKit.Bot/CommandMeta/CommandHelp.cs
@@ -23,8 +23,8 @@ public partial class CommandTree
     public static Command ConfigPing = new Command("config ping", "config ping <enable|disable>", "Changes your system's ping preferences");
     public static Command ConfigAutoproxyAccount = new Command("config autoproxy account", "autoproxy account [on|off]", "Toggles autoproxy globally for the current account");
     public static Command ConfigAutoproxyTimeout = new Command("config autoproxy timeout", "autoproxy timeout [<duration>|off|reset]", "Sets the latch timeout duration for your system");
-    public static Command ConfigMemberDefaultPrivacy = new("config member private", "config member private [on|off]", "Sets whether member privacy is automatically set to private when creating a new member");
-    public static Command ConfigGroupDefaultPrivacy = new("config group private", "config group private [on|off]", "Sets whether group privacy is automatically set to private when creating a new group");
+    public static Command ConfigMemberDefaultPrivacy = new("config private member", "config private member [on|off]", "Sets whether member privacy is automatically set to private when creating a new member");
+    public static Command ConfigGroupDefaultPrivacy = new("config private group", "config private group [on|off]", "Sets whether group privacy is automatically set to private when creating a new group");
     public static Command AutoproxySet = new Command("autoproxy", "autoproxy [off|front|latch|member]", "Sets your system's autoproxy mode for the current server");
     public static Command MemberInfo = new Command("member", "member <member>", "Looks up information about a member");
     public static Command MemberNew = new Command("member new", "member new <name>", "Creates a new member");
