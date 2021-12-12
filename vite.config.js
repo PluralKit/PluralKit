1ea53dc26d3fd8f09a8ca70b8d274582a54be02f
Spectralitree <72747870+Spectralitree@users.noreply.github.com>
2021-12-12 12:05:24 +0100

    deploy to github pages maybe?

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  optimizeDeps: { exclude: ["svelte-navigator"] }
+  optimizeDeps: { exclude: ["svelte-navigator"] },
+  build: {
+    outDir: "docs"
+  }
 })
