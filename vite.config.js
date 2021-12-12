6df28966be356b6324af9a3cc6a14929d2d23b0b
Spectralitree <72747870+Spectralitree@users.noreply.github.com>
2021-12-12 12:56:26 +0100

    fix: actually fix spa routing

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import resolve from 'path';
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
@@ -5,10 +6,16 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte()],
   optimizeDeps: { exclude: ["svelte-navigator"] },
-  build: {
-    outDir: "docs"
-  },
   server: {
     https: true
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        404: '404.html'
+      },
+    },
+    outDir: "docs"
   }
 })
