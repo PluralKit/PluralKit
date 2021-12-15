1516d76bf44bcab676cf78de4689aac85c1e0056
Spectralitree <72747870+Spectralitree@users.noreply.github.com>
2021-12-15 13:52:33 +0100

    fix: https error on the dev server

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte()],
   optimizeDeps: { exclude: ["svelte-navigator"] },
-  server: {
-    https: true
-  },
   build: {
     rollupOptions: {
       input: {
