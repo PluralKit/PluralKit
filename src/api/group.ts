05b590e7b9c5732fb398f3ea07346d10b2265a7e
Spectralitree <72747870+Spectralitree@users.noreply.github.com>
2021-12-18 20:09:07 +0100

    feat: add created to group properties

--- src/api/group.ts
+++ src/api/group.ts
@@ -15,6 +15,7 @@ export default class Group {
     banner?: string;
     color?: string;
     privacy?: GroupPrivacy;
+    created?: string;
 
     constructor(data: any) {
         this.id = data.id;
@@ -25,6 +26,7 @@ export default class Group {
         this.icon = data.icon;
         this.banner = data.banner;
         this.color = data.color;
+        this.created = data.created;
         if (data.privacy) {
             this.privacy = {}
             this.privacy.description_privacy = data.privacy.description_privacy;
