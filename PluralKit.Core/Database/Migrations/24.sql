e73af85e5c3584702bf2d1c95ef6abf787fef1d2
spiral <spiral@spiral.sh>
2022-01-11 09:46:20 -0500

    fix typo in migration

--- PluralKit.Core/Database/Migrations/24.sql
+++ PluralKit.Core/Database/Migrations/24.sql
@@ -2,7 +2,7 @@
 -- don't drop message rows when system/member are deleted
 
 alter table messages alter column member drop not null;
-alter table messages drop contstraint messages_member_fkey;
+alter table messages drop constraint messages_member_fkey;
 alter table messages
     add constraint messages_member_fkey
     foreign key (member) references members(id) on delete set null;
