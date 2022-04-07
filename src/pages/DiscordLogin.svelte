e0e0ab57746a9b65364bf3149b2e9aa0e0636ce8
spiral <spiral@spiral.sh>
2022-04-07 09:22:35 -0400

    feat: add Discord login

---
+++ src/pages/DiscordLogin.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import { Container, Row, Col } from 'sveltestrap';
+    import { onMount } from 'svelte';
+
+    import api from '../api';
+
+    let text = "Loading...";
+    
+    onMount(async () =>
+    {
+        const params = new URLSearchParams(window.location.search);
+        const paramkeys = [...params.keys()];
+        if (paramkeys.includes("code"))
+        {
+            const res = await api().private.discord.callback.post({ data: { code: params.get("code"), redirect_domain: window.location.origin } });
+            localStorage.setItem("pk-token", res.token);
+            localStorage.setItem("pk-user", JSON.stringify(res.system));
+            window.location.href = window.location.origin;
+        }
+        else
+        {
+            text = "Error: " + params.get("error_description");
+        }
+    });
+</script>
+
+<Container>
+    <Row>
+        <Col class="mx-auto" xs={12} lg={11} xl={10}>
+                {text}
+        </Col>
+    </Row>
+</Container>
