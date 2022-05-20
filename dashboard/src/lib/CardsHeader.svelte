4df4aafb7e4b8c20559012132711801a0237035a
Draconizations <jake.fulminated@gmail.com>
2022-05-20 12:09:40 +0200

    fix(dashboard): incorrect alt text in avatar modal

--- dashboard/src/lib/CardsHeader.svelte
+++ dashboard/src/lib/CardsHeader.svelte
@@ -50,7 +50,7 @@
         </div>
         <Modal isOpen={avatarOpen} toggle={toggleAvatarModal}>
             <div slot="external" on:click={toggleAvatarModal} style="height: 100%;  max-width: 640px; width: 100%; margin-left: auto; margin-right: auto; display: flex;">
-                <Image style="display: block; margin: auto;" src={icon_url} thumbnail alt="Your system avatar" />
+                <Image style="display: block; margin: auto;" src={icon_url} thumbnail alt={altText}/>
             </div>
         </Modal>
     </CardTitle>
