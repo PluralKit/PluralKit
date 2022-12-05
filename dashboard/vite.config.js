3c213495728e16cef7610165c86c6aff30a7f56d
spiral <spiral@spiral.sh>
2022-12-05 09:53:05 +0000

    fix(dash): split out highlight.js into a separate minified file

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -9,5 +9,18 @@ export default defineConfig({
   build: {
     outDir: "dist",
     sourcemap: true,
+    rollupOptions: {
+      manualChunks(filename) {
+        filename = filename.split("node_modules");
+        if (filename.length < 2) return 'index';
+        else filename = filename[1];
+
+        // this is really big and makes the map size go over the sentry file cache limit
+        if (filename.includes("highlight.js")) return 'vendor-0';
+
+        return 'vendor-1';
+        // return `vendor-${filename.charCodeAt(1) % 2}`;
+      }
+    }  
   }
 })
