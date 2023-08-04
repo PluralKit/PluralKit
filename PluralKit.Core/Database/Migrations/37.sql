5fbb83517648abc5d78b60a52f471000db670bb5
rladenson <78043712+rladenson@users.noreply.github.com>
2023-08-04 01:56:09 -0600

    fix: add missing constraint for proxy tag privacy

--- PluralKit.Core/Database/Migrations/37.sql
+++ PluralKit.Core/Database/Migrations/37.sql
@@ -2,5 +2,6 @@
 -- add proxy tag privacy
 
 alter table members add column proxy_privacy integer not null default 1;
+alter table members add constraint members_proxy_privacy_check check (proxy_privacy = ANY (ARRAY[1,2]));
 
 update info set schema_version = 37;
