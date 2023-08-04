f6e0a573db9e64c38ae513a4c16e06a9b03717b6
rladenson <78043712+rladenson@users.noreply.github.com>
2023-08-04 01:53:00 -0600

    fix: add constraint for system name and avatar privacy

---
+++ PluralKit.Core/Database/Migrations/37.sql
@@ -0,0 +1,7 @@
+-- database version 37
+-- add missing privacy constraints
+
+alter table systems add constraint systems_name_privacy_check check (name_privacy = ANY (ARRAY[1,2]));
+alter table systems add constraint systems_avatar_privacy_check check (avatar_privacy = ANY (ARRAY[1,2]));
+
+update info set schema_version = 37;
