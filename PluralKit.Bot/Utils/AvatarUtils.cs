7880d3baf9ea33d3bf9ce8059f417b0ecb78d0de
Astrid <mail+github@astr.cc>
2024-02-22 06:38:15 +0100

    Also send gifs over to avatar service

--- PluralKit.Bot/Utils/AvatarUtils.cs
+++ PluralKit.Bot/Utils/AvatarUtils.cs
@@ -63,7 +63,7 @@ public static class AvatarUtils
     // This lets us add resizing parameters to "borrow" their media proxy server to downsize the image
     // which in turn makes it more likely to be underneath the size limit!
     private static readonly Regex DiscordCdnUrl =
-        new(@"^https?://(?:cdn\.discordapp\.com|media\.discordapp\.net)/attachments/(\d{17,19})/(\d{17,19})/([^/\\&\?]+)\.(png|jpe?g|webp)(?:\?(?<query>.*))?$", RegexOptions.IgnoreCase);
+        new(@"^https?://(?:cdn\.discordapp\.com|media\.discordapp\.net)/attachments/(\d{17,19})/(\d{17,19})/([^/\\&\?]+)\.(png|jpe?g|gif|webp)(?:\?(?<query>.*))?$", RegexOptions.IgnoreCase);
 
     private static readonly string DiscordMediaUrlReplacement =
         "https://media.discordapp.net/attachments/$1/$2/$3.$4?width=256&height=256";
