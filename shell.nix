8210cb23f62a8bf53cdb925bafa5878e8f7d6e69
alyssa <alyssa@mailc.net>
2024-08-04 07:25:02 +0900

    feat: add basic shell.nix

---
+++ shell.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    cargo rust-analyzer rustfmt
+    gcc
+    protobuf
+    dotnet-sdk_6
+    go
+    nodejs yarn
+  ];
+}
