c26b61ce626155a81b27692389ca31ef68f9555f
rladenson <github@petalladenson.com>
2024-10-02 20:54:49 -0600

    feat: add pk;dash command that links to the dashboard

--- PluralKit.Bot/CommandMeta/CommandTree.cs
+++ PluralKit.Bot/CommandMeta/CommandTree.cs
@@ -105,6 +105,8 @@ public partial class CommandTree
                 return ctx.Execute<Random>(GroupRandom, r => r.Group(ctx, ctx.System));
             else
                 return ctx.Execute<Random>(MemberRandom, m => m.Member(ctx, ctx.System));
+        if (ctx.Match("dashboard", "dash"))
+            return ctx.Reply("The PluralKit dashboard is at <https://dash.pluralkit.me>");
 
         // remove compiler warning
         return ctx.Reply(
