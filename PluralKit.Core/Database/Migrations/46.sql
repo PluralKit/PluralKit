87196e32977e221d7dda9a9267746484329193b2
alyssa <alyssa@mailc.net>
2024-11-10 04:29:06 +0900

    fix: correct number in migration 46 (merge issue)

--- PluralKit.Core/Database/Migrations/46.sql
+++ PluralKit.Core/Database/Migrations/46.sql
@@ -1,4 +1,4 @@
--- database version 45
+-- database version 46
 -- adds banner privacy
 
 alter table members add column banner_privacy int not null default 1 check (banner_privacy = ANY (ARRAY[1,2]));
@@ -9,4 +9,4 @@ update members set banner_privacy = 2 where description_privacy = 2;
 update groups set banner_privacy = 2 where description_privacy = 2;
 update systems set banner_privacy = 2 where description_privacy = 2;
 
-update info set schema_version = 45;
+update info set schema_version = 46;
