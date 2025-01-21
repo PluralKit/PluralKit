bf5e448aad096baf3de3b9f340f873d118449299
dusk <y.bera003.06@protonmail.com>
2025-01-21 23:57:10 +0900

    fix: add the helper methods for resolving flag values

--- crates/command_definitions/src/help.rs
+++ crates/command_definitions/src/help.rs
@@ -3,7 +3,9 @@ use super::*;
 pub fn cmds() -> impl Iterator<Item = Command> {
     let help = ["help", "h"];
     [
-        command!([help] => "help").help("Shows the help command"),
+        command!([help] => "help")
+            .value_flag("foo", OpaqueString) // todo: just for testing
+            .help("Shows the help command"),
         command!([help, "commands"] => "help_commands").help("help commands"),
         command!([help, "proxy"] => "help_proxy").help("help proxy"),
     ]
