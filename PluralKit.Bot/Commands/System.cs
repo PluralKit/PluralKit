dfd9ea553a8fa8177150a6231c86ea5c3cf11070
rladenson <github@petalladenson.com>
2025-04-27 20:43:27 -0600

    feat: show notice of ToS on system creation

--- PluralKit.Bot/Commands/System.cs
+++ PluralKit.Bot/Commands/System.cs
@@ -38,10 +38,13 @@ public class System
             .Field(new Embed.Field("Getting Started",
                 "New to PK? Check out our Getting Started guide on setting up members and proxies: https://pluralkit.me/start\n" +
                 $"Otherwise, type `{ctx.DefaultPrefix}system` to view your system and `{ctx.DefaultPrefix}system help` for more information about commands you can use."))
-            .Field(new Embed.Field($"{Emojis.Warn} Notice {Emojis.Warn}", "PluralKit is a bot meant to help you share information about your system. " +
+            .Field(new Embed.Field($"{Emojis.Warn} Notice: Public By Default {Emojis.Warn}", "PluralKit is a bot meant to help you share information about your system. " +
                 "Member descriptions are meant to be the equivalent to a Discord About Me. Because of this, any info you put in PK is **public by default**.\n" +
                 "Note that this does **not** include message content, only member fields. For more information, check out " +
                 "[the privacy section of the user guide](https://pluralkit.me/guide/#privacy). "))
+            .Field(new Embed.Field($"{Emojis.Warn} Notice: Implicit Acceptance of ToS {Emojis.Warn}", "By using the PluralKit bot you implicitly agree to our " +
+                "[Terms of Service](https://pluralkit.me/terms-of-service/). For questions please ask in our [support server](<https://discord.gg/PczBt78>) or " +
+                "email legal@pluralkit.me"))
             .Field(new Embed.Field("System Recovery", "In the case of your Discord account getting lost or deleted, the PluralKit staff can help you recover your system. " +
                 "In order to do so, we will need your **PluralKit token**. This is the *only* way you can prove ownership so we can help you recover your system. " +
                 $"To get it, run `{ctx.DefaultPrefix}token` and then store it in a safe place.\n\n" +
