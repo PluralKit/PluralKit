0d29122109e343ac552e798953b91661e486fa9b
alyssa <alyssa@mailc.net>
2025-06-08 20:27:35 +0000

    feat(api): try harder at checking whether user is in guild

--- PluralKit.API/ApiConfig.cs
+++ PluralKit.API/ApiConfig.cs
@@ -7,4 +7,5 @@ public class ApiConfig
     public string? ClientSecret { get; set; }
     public bool TrustAuth { get; set; } = false;
     public string? AvatarServiceUrl { get; set; }
+    public bool SearchGuildSettings = false;
 }
