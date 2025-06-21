a81841f9f59e12adb10d68ad0c4ed2c21d853a1e
alyssa <alyssa@mailc.net>
2025-06-21 21:31:47 +0000

    fix: remove "concurrently" from migration

--- PluralKit.Core/Database/Migrations/52.sql
+++ PluralKit.Core/Database/Migrations/52.sql
@@ -1,8 +1,8 @@
 -- database version 52
 -- messages db updates
 
-create index concurrently messages_by_original on messages(original_mid);
-create index concurrently messages_by_sender on messages(sender);
+create index messages_by_original on messages(original_mid);
+create index messages_by_sender on messages(sender);
 
 create table command_messages (
     mid bigint primary key,
@@ -12,7 +12,7 @@ create table command_messages (
     original_mid bigint not null
 );
 
-create index concurrently command_messages_by_original on command_messages(original_mid);
-create index concurrently command_messages_by_sender on command_messages(sender);
+create index command_messages_by_original on command_messages(original_mid);
+create index command_messages_by_sender on command_messages(sender);
 
 update info set schema_version = 52;
