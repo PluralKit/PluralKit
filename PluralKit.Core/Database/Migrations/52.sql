497bee9487e53a1c521665a617ff22d61e0faff8
alyssa <alyssa@mailc.net>
2025-07-14 13:10:33 +0000

    fix: move old command_messages table in migration 52

--- PluralKit.Core/Database/Migrations/52.sql
+++ PluralKit.Core/Database/Migrations/52.sql
@@ -4,6 +4,9 @@
 create index messages_by_original on messages(original_mid);
 create index messages_by_sender on messages(sender);
 
+-- remove old table from database version 11
+alter table command_messages rename to command_messages_old;
+
 create table command_messages (
     mid bigint primary key,
     channel bigint not null,
