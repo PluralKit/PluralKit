83dd880374b15365f7f94fa7c04312d60d201f84
alyssa <alyssa@mailc.net>
2025-10-15 21:15:54 +0000

    chore: move app-commands script to rust

--- scripts/app-commands/common/types.py
+++
@@ -1,7 +0,0 @@
-class MessageCommand(dict):
-    COMMAND_TYPE = 3
-
-    def __init__(self, name):
-        super().__init__()
-        self["type"] = self.__class__.COMMAND_TYPE
-        self["name"] = name
--- scripts/dump-db.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker-compose -f "$(dirname $0)/../docker-compose.yml" exec -T -u postgres db pg_dump postgres
-
