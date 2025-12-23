f1471088d2c32f874aba77628a5f866c39174a69
alyssa <alyssa@mailc.net>
2025-12-23 00:45:45 -0500

    feat: premium service boilerplate

--- crates/api/src/main.rs
+++ crates/api/src/main.rs
@@ -1,135 +1,95 @@
-use auth::{AuthState, INTERNAL_APPID_HEADER, INTERNAL_SYSTEMID_HEADER};
+use api::ApiContext;
+use auth::AuthState;
 use axum::{
     Extension, Router,
     body::Body,
-    extract::{Request as ExtractRequest, State},
+    extract::Request as ExtractRequest,
     http::Uri,
-    response::{IntoResponse, Response},
     routing::{delete, get, patch, post},
 };
-use hyper_util::{
-    client::legacy::{Client, connect::HttpConnector},
-    rt::TokioExecutor,
-};
+use hyper_util::{client::legacy::connect::HttpConnector, rt::TokioExecutor};
 use libpk::config;
 use tracing::{info, warn};
 
-use pk_macros::api_endpoint;
+use crate::proxyer::Proxyer;
 
 mod auth;
 mod endpoints;
 mod error;
 mod middleware;
+mod proxyer;
 mod util;
-
-#[derive(Clone)]
-pub struct ApiContext {
-    pub db: sqlx::postgres::PgPool,
-    pub redis: fred::clients::RedisPool,
-
-    rproxy_uri: String,
-    rproxy_client: Client<HttpConnector, Body>,
-}
-
-#[api_endpoint]
-async fn rproxy(
-    Extension(auth): Extension<AuthState>,
-    State(ctx): State<ApiContext>,
-    mut req: ExtractRequest<Body>,
-) -> Response {
-    let path = req.uri().path();
-    let path_query = req
-        .uri()
-        .path_and_query()
-        .map(|v| v.as_str())
-        .unwrap_or(path);
-
-    let uri = format!("{}{}", ctx.rproxy_uri, path_query);
-
-    *req.uri_mut() = Uri::try_from(uri).unwrap();
-
-    let headers = req.headers_mut();
-
-    headers.remove(INTERNAL_SYSTEMID_HEADER);
-    headers.remove(INTERNAL_APPID_HEADER);
-
-    if let Some(sid) = auth.system_id() {
-        headers.append(INTERNAL_SYSTEMID_HEADER, sid.into());
-    }
-
-    if let Some(aid) = auth.app_id() {
-        headers.append(INTERNAL_APPID_HEADER, aid.into());
-    }
-
-    Ok(ctx.rproxy_client.request(req).await?.into_response())
-}
 
 // this function is manually formatted for easier legibility of route_services
 #[rustfmt::skip]
-fn router(ctx: ApiContext) -> Router {
+fn router(ctx: ApiContext, proxyer: Proxyer) -> Router {
+    let rproxy = |Extension(auth): Extension<AuthState>, req: ExtractRequest<Body>| {
+        proxyer.rproxy(auth, req)
+    };
+
     // processed upside down (???) so we have to put middleware at the end
     Router::new()
-        .route("/v2/systems/{system_id}", get(rproxy))
-        .route("/v2/systems/{system_id}", patch(rproxy))
+        .route("/v2/systems/{system_id}", get(rproxy.clone()))
+        .route("/v2/systems/{system_id}", patch(rproxy.clone()))
         .route("/v2/systems/{system_id}/settings", get(endpoints::system::get_system_settings))
-        .route("/v2/systems/{system_id}/settings", patch(rproxy))
+        .route("/v2/systems/{system_id}/settings", patch(rproxy.clone()))
 
-        .route("/v2/systems/{system_id}/members", get(rproxy))
-        .route("/v2/members", post(rproxy))
-        .route("/v2/members/{member_id}", get(rproxy))
-        .route("/v2/members/{member_id}", patch(rproxy))
-        .route("/v2/members/{member_id}", delete(rproxy))
+        .route("/v2/systems/{system_id}/members", get(rproxy.clone()))
+        .route("/v2/members", post(rproxy.clone()))
+        .route("/v2/members/{member_id}", get(rproxy.clone()))
+        .route("/v2/members/{member_id}", patch(rproxy.clone()))
+        .route("/v2/members/{member_id}", delete(rproxy.clone()))
 
-        .route("/v2/systems/{system_id}/groups", get(rproxy))
-        .route("/v2/groups", post(rproxy))
-        .route("/v2/groups/{group_id}", get(rproxy))
-        .route("/v2/groups/{group_id}", patch(rproxy))
-        .route("/v2/groups/{group_id}", delete(rproxy))
+        .route("/v2/systems/{system_id}/groups", get(rproxy.clone()))
+        .route("/v2/groups", post(rproxy.clone()))
+        .route("/v2/groups/{group_id}", get(rproxy.clone()))
+        .route("/v2/groups/{group_id}", patch(rproxy.clone()))
+        .route("/v2/groups/{group_id}", delete(rproxy.clone()))
 
-        .route("/v2/groups/{group_id}/members", get(rproxy))
-        .route("/v2/groups/{group_id}/members/add", post(rproxy))
-        .route("/v2/groups/{group_id}/members/remove", post(rproxy))
-        .route("/v2/groups/{group_id}/members/overwrite", post(rproxy))
+        .route("/v2/groups/{group_id}/members", get(rproxy.clone()))
+        .route("/v2/groups/{group_id}/members/add", post(rproxy.clone()))
+        .route("/v2/groups/{group_id}/members/remove", post(rproxy.clone()))
+        .route("/v2/groups/{group_id}/members/overwrite", post(rproxy.clone()))
 
-        .route("/v2/members/{member_id}/groups", get(rproxy))
-        .route("/v2/members/{member_id}/groups/add", post(rproxy))
-        .route("/v2/members/{member_id}/groups/remove", post(rproxy))
-        .route("/v2/members/{member_id}/groups/overwrite", post(rproxy))
+        .route("/v2/members/{member_id}/groups", get(rproxy.clone()))
+        .route("/v2/members/{member_id}/groups/add", post(rproxy.clone()))
+        .route("/v2/members/{member_id}/groups/remove", post(rproxy.clone()))
+        .route("/v2/members/{member_id}/groups/overwrite", post(rproxy.clone()))
 
-        .route("/v2/systems/{system_id}/switches", get(rproxy))
-        .route("/v2/systems/{system_id}/switches", post(rproxy))
-        .route("/v2/systems/{system_id}/fronters", get(rproxy))
+        .route("/v2/systems/{system_id}/switches", get(rproxy.clone()))
+        .route("/v2/systems/{system_id}/switches", post(rproxy.clone()))
+        .route("/v2/systems/{system_id}/fronters", get(rproxy.clone()))
 
-        .route("/v2/systems/{system_id}/switches/{switch_id}", get(rproxy))
-        .route("/v2/systems/{system_id}/switches/{switch_id}", patch(rproxy))
-        .route("/v2/systems/{system_id}/switches/{switch_id}/members", patch(rproxy))
-        .route("/v2/systems/{system_id}/switches/{switch_id}", delete(rproxy))
+        .route("/v2/systems/{system_id}/switches/{switch_id}", get(rproxy.clone()))
+        .route("/v2/systems/{system_id}/switches/{switch_id}", patch(rproxy.clone()))
+        .route("/v2/systems/{system_id}/switches/{switch_id}/members", patch(rproxy.clone()))
+        .route("/v2/systems/{system_id}/switches/{switch_id}", delete(rproxy.clone()))
 
-        .route("/v2/systems/{system_id}/guilds/{guild_id}", get(rproxy))
-        .route("/v2/systems/{system_id}/guilds/{guild_id}", patch(rproxy))
+        .route("/v2/systems/{system_id}/guilds/{guild_id}", get(rproxy.clone()))
+        .route("/v2/systems/{system_id}/guilds/{guild_id}", patch(rproxy.clone()))
 
-        .route("/v2/members/{member_id}/guilds/{guild_id}", get(rproxy))
-        .route("/v2/members/{member_id}/guilds/{guild_id}", patch(rproxy))
+        .route("/v2/members/{member_id}/guilds/{guild_id}", get(rproxy.clone()))
+        .route("/v2/members/{member_id}/guilds/{guild_id}", patch(rproxy.clone()))
 
-        .route("/v2/systems/{system_id}/autoproxy", get(rproxy))
-        .route("/v2/systems/{system_id}/autoproxy", patch(rproxy))
+        .route("/v2/systems/{system_id}/autoproxy", get(rproxy.clone()))
+        .route("/v2/systems/{system_id}/autoproxy", patch(rproxy.clone()))
 
-        .route("/v2/messages/{message_id}", get(rproxy))
+        .route("/v2/messages/{message_id}", get(rproxy.clone()))
 
         .route("/v2/bulk", post(endpoints::bulk::bulk))
 
-        .route("/private/bulk_privacy/member", post(rproxy))
-        .route("/private/bulk_privacy/group", post(rproxy))
-        .route("/private/discord/callback", post(rproxy))
+        .route("/private/bulk_privacy/member", post(rproxy.clone()))
+        .route("/private/bulk_privacy/group", post(rproxy.clone()))
+        .route("/private/discord/callback", post(rproxy.clone()))
         .route("/private/discord/callback2", post(endpoints::private::discord_callback))
         .route("/private/discord/shard_state", get(endpoints::private::discord_state))
         .route("/private/dash_views", post(endpoints::private::dash_views))
         .route("/private/dash_view/{id}", get(endpoints::private::dash_view))
         .route("/private/stats", get(endpoints::private::meta))
 
-        .route("/v2/systems/{system_id}/oembed.json", get(rproxy))
-        .route("/v2/members/{member_id}/oembed.json", get(rproxy))
-        .route("/v2/groups/{group_id}/oembed.json", get(rproxy))
+        .route("/v2/systems/{system_id}/oembed.json", get(rproxy.clone()))
+        .route("/v2/members/{member_id}/oembed.json", get(rproxy.clone()))
+        .route("/v2/groups/{group_id}/oembed.json", get(rproxy.clone()))
 
         .layer(axum::middleware::from_fn_with_state(
             if config.api().use_ratelimiter {
@@ -161,15 +121,14 @@ async fn main() -> anyhow::Result<()> {
     let rproxy_client = hyper_util::client::legacy::Client::<(), ()>::builder(TokioExecutor::new())
         .build(HttpConnector::new());
 
-    let ctx = ApiContext {
-        db,
-        redis,
-
+    let proxyer = Proxyer {
         rproxy_uri: rproxy_uri[..rproxy_uri.len() - 1].to_string(),
         rproxy_client,
     };
 
-    let app = router(ctx);
+    let ctx = ApiContext { db, redis };
+
+    let app = router(ctx, proxyer);
 
     let addr: &str = libpk::config.api().addr.as_ref();
 
