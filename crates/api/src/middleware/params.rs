30262cbf29b38270629193918f6bad7be1786d94
Ayake <golly.ticker@gmail.com>
2026-03-23 23:55:16 +0100

    perf(api): Simplify and reduce unnecessary requests. Distinguish requested and authenticated system clearly.

--- crates/api/src/middleware/params.rs
+++ crates/api/src/middleware/params.rs
@@ -6,12 +6,18 @@ use axum::{
     routing::url_params::UrlParams,
 };
 
-use sqlx::{Postgres, types::Uuid};
+use sqlx::{Postgres, prelude::FromRow, types::Uuid};
 use tracing::error;
 
 use crate::auth::AuthState;
 use crate::{ApiContext, util::json_err};
-use pluralkit_models::PKSystem;
+use pluralkit_models::SystemId;
+
+/// The system about which the current request is
+#[derive(Clone, FromRow)]
+pub struct RequestAboutSystem {
+    pub id: SystemId,
+}
 
 // move this somewhere else
 fn parse_hid(hid: &str) -> String {
@@ -52,69 +58,36 @@ pub async fn params(State(ctx): State<ApiContext>, mut req: Request, next: Next)
                         .into();
                     };
 
-                    match sqlx::query_as::<Postgres, PKSystem>(
-                        "select * from systems where id = $1",
-                    )
-                    .bind(system_id)
-                    .fetch_optional(&ctx.db)
-                    .await
-                    {
-                        Ok(Some(system)) => {
-                            req.extensions_mut().insert(system);
-                        }
-                        Ok(None) => {
-                            error!(
-                                ?system_id,
-                                "could not find previously authenticated system in db"
-                            );
-                            return json_err(
-                                StatusCode::INTERNAL_SERVER_ERROR,
-                                r#"{"message": "500: Internal Server Error", "code": 0}"#
-                                    .to_string(),
-                            );
-                        }
-                        Err(err) => {
-                            error!(
-                                ?err,
-                                "failed to query previously authenticated system in db"
-                            );
-                            return json_err(
-                                StatusCode::INTERNAL_SERVER_ERROR,
-                                r#"{"message": "500: Internal Server Error", "code": 0}"#
-                                    .to_string(),
-                            );
-                        }
-                    }
+                    req.extensions_mut()
+                        .insert(RequestAboutSystem { id: system_id });
                 }
                 id => {
-                    println!("a {id}");
-                    match match Uuid::parse_str(id) {
-                        Ok(uuid) => sqlx::query_as::<Postgres, PKSystem>(
-                            "select * from systems where uuid = $1",
+                    println!("params.rs. lookup {id}");
+                    let query = match Uuid::parse_str(id) {
+                        Ok(uuid) => sqlx::query_as::<Postgres, RequestAboutSystem>(
+                            "select id from systems where uuid = $1",
                         )
                         .bind(uuid),
                         Err(_) => match id.parse::<i64>() {
-                            Ok(parsed) => sqlx::query_as::<Postgres, PKSystem>(
-                                "select * from systems where id = (select system from accounts where uid = $1)"
+                            Ok(parsed) => sqlx::query_as::<Postgres, RequestAboutSystem>(
+                                "select id from systems where id = (select system from accounts where uid = $1)"
                             )
                             .bind(parsed),
-                            Err(_) => sqlx::query_as::<Postgres, PKSystem>(
-                                "select * from systems where hid = $1",
+                            Err(_) => sqlx::query_as::<Postgres, RequestAboutSystem>(
+                                "select id from systems where hid = $1",
                             )
                             .bind(parse_hid(id))
                         },
-                    }
-                    .fetch_optional(&ctx.db)
-                    .await
-                    {
-                        Ok(Some(system)) => {
-                            req.extensions_mut().insert(system);
+                    };
+                    match query.fetch_optional(&ctx.db).await {
+                        Ok(Some(request_system)) => {
+                            req.extensions_mut().insert(request_system);
                         }
                         Ok(None) => {
                             return json_err(
                                 StatusCode::NOT_FOUND,
                                 r#"{"message":"System not found.","code":20001}"#.to_string(),
-                            )
+                            );
                         }
                         Err(err) => {
                             error!(?err, ?id, "failed to query system from path in db");
