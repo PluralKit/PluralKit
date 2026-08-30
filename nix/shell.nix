da49d4c87d1cc424b3bb6c23e46600129ee9337c
dish <pyrox@pyrox.dev>
2026-08-30 12:52:16 -0400

    flake: Add postgresql to devShell and set default variables (#796)
    Sets PGHOST and PGUSER so that you can run `psql` without arguments and
    get a working psql connection. Also adds the `psql` CLI to the devshell
    so that it's not needed to be added externally.

--- nix/shell.nix
+++ nix/shell.nix
@@ -11,11 +11,16 @@
       rustOutputs = config.nci.outputs;
       rustDevshell = rustOutputs."pluralkit-services".devShell;
 
-      commonTools = with pkgs; [ nixd ];
+      commonTools = with pkgs; [
+        nixd
+        postgresql
+      ];
       withCommon =
         shell:
         shell.overrideAttrs (old: {
           nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ commonTools;
+          env.PGHOST = "127.0.0.1";
+          env.PGUSER = "postgres";
         });
 
       docs = pkgs.mkShellNoCC {
