b17261ee07492ebd168f186eff689d99b597fb43
Petal Ladenson <github@petalladenson.com>
2026-09-14 15:25:52 -0600

    style: dotnet format

--- PluralKit.Bot/Services/EmbedService.cs
+++ PluralKit.Bot/Services/EmbedService.cs
@@ -183,7 +183,7 @@ public class EmbedService
             {
                 Type = ComponentType.Container,
                 AccentColor = system.Color?.ToDiscordColor(),
-                Components = [ ..header, ..switchComponent, linkedAccounts, ..descComponents ],
+                Components = [.. header, .. switchComponent, linkedAccounts, .. descComponents],
             },
             new MessageComponent()
             {
@@ -475,7 +475,7 @@ public class EmbedService
             {
                 Type = ComponentType.Container,
                 AccentColor = member.Color?.ToDiscordColor(),
-                Components = [ ..header, ..extraData, ..descComponents ],
+                Components = [.. header, .. extraData, .. descComponents],
             },
             new MessageComponent()
             {
@@ -670,7 +670,7 @@ public class EmbedService
             {
                 Type = ComponentType.Container,
                 AccentColor = target.Color?.ToDiscordColor(),
-                Components = [ ..header, ..descComponents ],
+                Components = [.. header, .. descComponents],
             },
             new MessageComponent()
             {
@@ -928,11 +928,11 @@ public class EmbedService
 
         return [
             new MessageComponent()
-    {
-        Type = ComponentType.Container,
+            {
+                Type = ComponentType.Container,
                 Components = [
                     header,
-                    ..body
+                    .. body
                 ]
             },
             footer
@@ -1115,10 +1115,11 @@ public class EmbedService
         };
 
         return [
-            new MessageComponent(){
+            new MessageComponent()
+            {
                 Type = ComponentType.Container,
                 Components = [
-                    ..body
+                    .. body
                 ]
             },
             footer
